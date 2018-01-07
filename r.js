0bdec9de91b5f1a05f8bfb231566eeac6980116a
Gaku <rxf10242@gmail.com>
2018-01-07 11:49:21 +0900

    18.1.7

--- r.js
+++ r.js
@@ -1,3 +1,3 @@
-var s = '<iframe src="http://rcm-jp.amazon.co.jp/e/cm?t=xfinder-22&o=9&p=20&l=ur1&category=amazonrotate&f=ifr" width="120" height="90" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>';
+var s = '<iframe src="//rcm-jp.amazon.co.jp/e/cm?t=xfinder-22&o=9&p=20&l=ur1&category=amazonrotate&f=ifr" width="120" height="90" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>';
 
 document.write(s);
